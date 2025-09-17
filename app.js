48cbfafc30ab1dfd5dc88710fec7c50689ba81b2
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-17 17:49:43 +0500

    more added

---
+++ app.js
@@ -0,0 +1,25 @@
+// ------------------------Chapter 21-25-------------------------------------
+
+// Question:01:-
+
+// var firstName = prompt("Enter your first Name");
+// var lastName = prompt("Enter your last Name");
+// var fullName = firstName+ " " +lastName;
+
+// alert("Welcome to my website Mr/Ms "+fullName);
+
+
+
+// Question:02:-
+
+// var phoneName = prompt("Enter your favourite phone name");
+// var length = phoneName.length;
+
+// document.write("<h2> My favourite phone is: ",phoneName,"</h2>");
+// document.write("<h2> Lenght of string:",length);
+
+// Question:03:-
+
+
+
+
